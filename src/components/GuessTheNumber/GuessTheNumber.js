120e113a47a1257e52cc2a7da37411e9cd8b1995
Ashik <9779540915aashik@gmail.com>
2024-07-05 19:48:59 +0530

    Done today - 5 july

--- src/components/GuessTheNumber/GuessTheNumber.js
+++ src/components/GuessTheNumber/GuessTheNumber.js
@@ -4,17 +4,26 @@ import React, { useState } from 'react'
 let getRandom = (min, max) => {
   return Math.floor(Math.random() * (max - min) + 1);
 }
-
-const random = getRandom(1, 10);
+let max = 10;
+let random = getRandom(1, max);
 console.log(random)
+let win = false;
 
+//component starts here
 const GuessTheNumber = () => {
 
   const [number, setNumber] = useState(0)
   const [guessedNum, setGuessedNum] = useState("")
+  const [message, setMessage] = useState("")
+  const [guessCount, setGuessCount] = useState(0)
 
   let handleChange = (event) => {
-    if (event.target.value === '') { console.log("please enter a number"); return }
+    if (event.target.value === '') {
+      setGuessedNum('')
+      setMessage("Please Enter A Number")
+      return
+    }
+    setMessage("");
     let num = event.target.value;
     num = Number.parseInt(num)
     setGuessedNum(num)
@@ -23,28 +32,50 @@ const GuessTheNumber = () => {
 
   let handleSubmit = () => {
     setGuessedNum("")
-    
-    if (number === 0 || guessedNum === "") {
-      console.log("Please Enter A Number")
-    } else if (guessedNum === number) {
-      console.log("Your guess is correct")
-    } else if (guessedNum < number) {
-      console.log("Your guess is less than the number")
-    } else {
-      console.log("Your guess is greater than the number")
+    setGuessCount(guessCount+1)
+
+    if (guessCount >= max) {
+      setMessage("You Lose")
+      return
     }
+
+    if (number === 0 || guessedNum === "") {
+      setMessage("Please Enter A Number")
+    } else if (guessedNum === number) {
+      setMessage("Your guess is correct")
+      win = true;
+    } else if (guessedNum < number) {
+      setMessage("Your guess is less than the number")
+    } else {
+      setMessage("Your guess is greater than the number")
+    }
+  }
+
+  let handleRetry = ()=>{
+    random = getRandom(1, max);
+    setGuessCount(0)
+    win = false;
+    setMessage("")
+    console.log(random)
   }
 
   return (
     <div className="container">
       <div className="title">
-        This is a Guess The Number Game. <br /> The Number is between 1 to 10
+        This is a Guess The Number Game. <br /> The Number is between 1 to {max} <br /> Max try = 10
       </div>
+      {
+        message ? <div className="message"> {message}  <br />{win!=true?<span>Attempts : {guessCount}</span>:""}</div> : ''
+      }
+      {
+        win ? <div className="win"> Your Score : {max - guessCount} <br /> Attempts : {guessCount}</div> : ""
+      }
       <div className="content">
         <input type="number" name='number' onChange={handleChange} value={guessedNum} />
       </div>
       <div className="btns">
         <button onClick={handleSubmit}>check</button>
+        <button onClick={handleRetry}>Retry</button>
       </div>
     </div>
   )
